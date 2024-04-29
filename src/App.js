f7684038525615092e1c6692ae3d02ed494996fe
react4four <react.four@smartivemedia.com>
2024-04-30 01:46:51 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from './Button';
 
 function App() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Button Sub-module</h1>
+      <Button onClick={handleClick}>Click Me</Button>
     </div>
   );
 }
