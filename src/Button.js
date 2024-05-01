f4fbf861fb36b7e6c6c121142e78882802b7b576
Esraa Anwar <esraa.anwar@tafweela.com>
2024-05-01 11:52:40 +0300

    edit btn

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,9 @@
 // Button.js
 import React from 'react';
-
+import "./button.css"
 const Button = ({ onClick, children }) => {
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick} className='btn'>
             {children}
         </button>
     );
